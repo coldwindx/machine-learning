6db75124014670ae6f3f398befbf5bd1eb725bea
coldwindx <15653913658@163.com>
2022-07-28 10:12:10 +0800

    语言模型

--- 《动手学习深度学习》/helper.py
+++ 《动手学习深度学习》/helper.py
@@ -1,6 +1,9 @@
 import os
+import re
 import hashlib
 import requests
+import random
+import collections
 import numpy as np
 
 import torch
@@ -15,6 +18,7 @@ from torchvision import transforms
 
 DATA_HUB = dict()
 DATA_URL = 'http://d2l-data.s3-accelerate.amazonaws.com/'
+DATA_HUB['time_machine'] = (DATA_URL + 'time_machine.txt', '090b5e7e70c295757f55df93cb0a180b9691891a')
 
 
 def set_axes(axes, xlabel, ylabel, xlim, ylim, xscale, yscale, legend):
@@ -70,10 +74,24 @@ class Loader:
         return train_iter, test_iter
 
     @staticmethod
+    def load_data_time_machine(batch_size, num_steps,
+                            use_random_iter=False, max_tokens=10000):
+        """返回时光机器数据集的迭代器和词表"""
+        data_iter = SeqDataLoader(
+            batch_size, num_steps, use_random_iter, max_tokens)
+        return data_iter, data_iter.vocab
+
+    @staticmethod
     def load_array(data_arrays, batch_size, shuffle = True):
         datasets = torch.utils.data.TensorDataset(*data_arrays)
         return torch.utils.data.DataLoader(datasets, batch_size, shuffle=shuffle)
     
+    @staticmethod
+    def read_time_machine():
+        with open(Loader.download('time_machine'), 'r') as f:
+            lines = f.readlines()
+        return [re.sub('[^A-Za-z]+', ' ', line).strip().lower() for line in lines]
+
     @staticmethod
     def download(url, folder = '../data', sha1_hash = None):
         if not url.startswith('http'):
@@ -255,6 +273,101 @@ class Evaluater:
             l = loss(out, y)
             metric.add(l.sum(), l.numel())
         return metric[0] / metric[1]
+
+def count_corpus(tokens):
+    '''统计次元频率'''
+    if len(tokens) == 0 or isinstance(tokens[0], list):
+        tokens = [token for line in tokens for token in line]
+    return collections.Counter(tokens)
+
+class Vocab:
+    @staticmethod
+    def tokenize(lines, token = 'word'):
+        if token == 'word':
+            return [line.split() for line in lines]
+        if token == 'char':
+            return [list(line) for line in lines]
+        print('错误：未知词元类型：' + token)
+
+    def __init__(self, tokens, min_freq = 0, reserved_tokens = None) -> None:
+        if tokens is None:
+            tokens = []
+        if reserved_tokens is None:
+            reserved_tokens = []
+        counter = count_corpus(tokens)
+        self._token_freqs = sorted(counter.items(), key=lambda x: x[1], reverse=True)
+
+        self.idx_to_token = ['<unk>'] + reserved_tokens
+        self.token_to_idx = { token: idx for idx, token in enumerate(self.idx_to_token) }
+
+        for token, freq in self._token_freqs:
+            # _token_freqs有序
+            if freq < min_freq:
+                break
+            if token not in self.token_to_idx:
+                self.idx_to_token.append(token)
+                self.token_to_idx[token] = len(self.idx_to_token) - 1
+    def __len__(self):
+        return len(self.idx_to_token)
+    def __getitem__(self, tokens):
+        if not isinstance(tokens, (list, tuple)):
+            return self.token_to_idx.get(tokens, self.unk)
+        return [self.__getitem__(token) for token in tokens]
+
+    def to_tokens(self, indices):
+        if not isinstance(indices, (list, tuple)):
+            return self.idx_to_token[indices]
+        return [self.idx_to_token[index] for index in indices]
+    
+    @property
+    def unk(self):
+        return 0
+    @property
+    def token_freqs(self):
+        return self._token_freqs
+
+class SeqDataLoader:  #@save
+    """加载序列数据的迭代器"""
+    def __init__(self, batch_size, num_steps, use_random_iter, max_tokens):
+        if use_random_iter:
+            self.data_iter_fn = SeqDataLoader._seq_data_iter_random
+        else:
+            self.data_iter_fn = SeqDataLoader._seq_data_iter_sequential
+        tokens = Vocab.tokenize(Loader.read_time_machine())
+        self.corpus = [token for line in tokens for token in line]
+        self.vocab = Vocab(self.corpus)
+        self.batch_size, self.num_steps = batch_size, num_steps
+
+    def __iter__(self):
+        return self.data_iter_fn(self.corpus, self.batch_size, self.num_steps)
+
+    @staticmethod
+    def _seq_data_iter_random(corpus, batch_size, num_steps):
+        corpus = corpus[random.randint(0, num_steps - 1):]
+        num_subseqs = (len(corpus) - 1) // num_steps
+        initial_indices = list(range(0, num_subseqs * num_steps, num_steps))
+        random.shuffle(initial_indices)
+        def data(pos):
+            return corpus[pos:pos + num_steps]
+        num_batches = num_subseqs // batch_size
+        for i in range(0, batch_size * num_batches, batch_size):
+            initial_indices_per_batch = initial_indices[i:i + batch_size]
+            X = [data(j) for j in initial_indices_per_batch]
+            Y = [data(j + 1) for j in initial_indices_per_batch]
+            yield torch.tensor(X), torch.tensor(Y)
+
+    @staticmethod
+    def _seq_data_iter_sequential(corpus, batch_size, num_steps):
+        offset = random.randint(0, num_steps)
+        num_tokens = ((len(corpus) - offset - 1) // batch_size) * batch_size
+        Xs = torch.tensor(corpus[offset:offset + num_tokens])
+        Ys = torch.tensor(corpus[offset + 1: offset + num_tokens + 1])
+        Xs, Ys = Xs.reshape((batch_size, -1)), Ys.reshape((batch_size, -1))
+        num_batchs = Xs.shape[1] // num_steps
+        for i in range(0, num_steps * num_batchs, num_steps):
+            X = Xs[:, i: i + num_steps]
+            Y = Ys[:, i: i + num_steps]
+            yield X, Y
 
 if __name__ =='__main__':
     a = Animator(xlabel='epochs', ylabel='metrics', metrics=['m'])
